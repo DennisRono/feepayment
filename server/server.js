ce7c4c6befe8b0923ea6efd5aba158920dcb51ac
DennisRono <finnneron1@gmail.com>
2022-09-20 12:11:27 +0300

    server changes

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ require('dotenv').config()
 
 //routes
 const authRoute = require('./routes/auth')
-const blogRoute = require('./routes/blog')
-const contactRoute = require('./routes/contact')
 
 //middlewares
 app.use(cors(options))
@@ -24,7 +22,5 @@ app.use(
 )
 
 app.use('/auth', authRoute)
-app.use('/blog', blogRoute)
-app.use('/contact', contactRoute)
 
 app.listen(PORT, ()=>{console.log(`server is running on port: ${PORT}`)})
