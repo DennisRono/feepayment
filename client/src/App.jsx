58b842a50333efba3ace55df5bcc0440c0edd51c
DennisRono <finnneron1@gmail.com>
2022-09-22 14:50:01 +0300

    breathe biggest running husband trail

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Auth from './routes/Auth'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Auth />
+    </Fragment>
+  )
+}
+
+export default App
---
+++ client/src/routes/Auth.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const auth = () => {
+  return (
+    <Fragment>
+        <div className="authentification">
+          
+        </div>
+    </Fragment>
+  )
+}
+
+export default auth
