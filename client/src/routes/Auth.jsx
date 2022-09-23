c58b32c33621f5def11dae63418ae048da5ff259
DennisRono <finnneron1@gmail.com>
2022-09-23 11:30:00 +0300

    explain attached will seeing volume

--- client/src/routes/Auth.jsx
+++ client/src/routes/Auth.jsx
@@ -1,113 +1,111 @@
 import React, { Fragment, useState } from 'react'
 import '../styles/css/auth.css'
 
-const auth = () => {
+const Auth = () => {
     const [active, setActive] = useState('login')
-    const activity = (activity) => {
-        (activity==='login')?setActive('login'):setActive('register')
-    }
   return (
     <Fragment>
         <div className="authentification">
-          <section class="contact">
-            <div class="contact-wrapper">
-                <div class="contFormSec">
-                    <h2 class="contact-header">Fees Payment System</h2>
+          <section className="contact">
+            <div className="contact-wrapper">
+                <div className="contFormSec">
+                    <h2 className="contact-header">Fees Payment System</h2>
                     <form action="contact.php" method="POST" className={(active==='register')?"registration-form":"hide-activity"}>
                         <h3>Register</h3>
-                        <div class="cont-group">
-                            <div class="cont-gr-flex">
-                                <div class="contPut">
-                                    <div class="user-input-wrp">
+                        <div className="cont-group">
+                            <div className="cont-gr-flex">
+                                <div className="contPut">
+                                    <div className="user-input-wrp">
                                         <br/>
-                                        <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="email" value=""/>
-                                        <span class="floating-label">First Name <span style={{color: "red"}}>*</span></span>
+                                        <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="email" value=""/>
+                                        <span className="floating-label">First Name <span style={{color: "red"}}>*</span></span>
                                     </div>
                                     <span id="id-err"></span>
                                 </div>
-                                <div class="contPut">
-                                    <div class="user-input-wrp">
+                                <div className="contPut">
+                                    <div className="user-input-wrp">
                                         <br/>
-                                        <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="phone" value=""/>
-                                        <span class="floating-label">Last Name <span style={{color: "red"}}>*</span></span>
+                                        <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="phone" value=""/>
+                                        <span className="floating-label">Last Name <span style={{color: "red"}}>*</span></span>
                                     </div>
                                     <span id="id-err"></span>
                                 </div>
                             </div>
-                            <div class="user-input-wrp">
+                            <div className="user-input-wrp">
                                 <br/>
-                                <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="name" value=""/>
-                                <span class="floating-label">Registration Number <span style={{color: "red"}}>*</span></span>
+                                <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="name" value=""/>
+                                <span className="floating-label">Registration Number <span style={{color: "red"}}>*</span></span>
                             </div>
                             <span id="id-err"></span>
-                            <div class="cont-gr-flex">
-                                <div class="contPut">
-                                    <div class="user-input-wrp">
+                            <div className="cont-gr-flex">
+                                <div className="contPut">
+                                    <div className="user-input-wrp">
                                         <br/>
-                                        <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="email" value=""/>
-                                        <span class="floating-label">School <span style={{color: "red"}}>*</span></span>
+                                        <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="email" value=""/>
+                                        <span className="floating-label">School <span style={{color: "red"}}>*</span></span>
                                     </div>
                                     <span id="id-err"></span>
                                 </div>
-                                <div class="contPut">
-                                    <div class="user-input-wrp">
+                                <div className="contPut">
+                                    <div className="user-input-wrp">
                                         <br/>
-                                        <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="phone" value=""/>
-                                        <span class="floating-label">Department <span style={{color: "red"}}>*</span></span>
-                                    </div>
-                                    <span id="id-err"></span>
-                                </div>
-                            </div>
-                            <div class="cont-gr-flex">
-                                <div class="contPut">
-                                    <div class="user-input-wrp">
-                                        <br/>
-                                        <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="email" value=""/>
-                                        <span class="floating-label">Semester <span style={{color: "red"}}>*</span></span>
-                                    </div>
-                                    <span id="id-err"></span>
-                                </div>
-                                <div class="contPut">
-                                    <div class="user-input-wrp">
-                                        <br/>
-                                        <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="phone" value=""/>
-                                        <span class="floating-label">Year of study <span style={{color: "red"}}>*</span></span>
+                                        <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="phone" value=""/>
+                                        <span className="floating-label">Department <span style={{color: "red"}}>*</span></span>
                                     </div>
                                     <span id="id-err"></span>
                                 </div>
                             </div>
-                            <div class="user-input-wrp">
+                            <div className="cont-gr-flex">
+                                <div className="contPut">
+                                    <div className="user-input-wrp">
+                                        <br/>
+                                        <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="email" value=""/>
+                                        <span className="floating-label">Semester <span style={{color: "red"}}>*</span></span>
+                                    </div>
+                                    <span id="id-err"></span>
+                                </div>
+                                <div className="contPut">
+                                    <div className="user-input-wrp">
+                                        <br/>
+                                        <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="phone" value=""/>
+                                        <span className="floating-label">Year of study <span style={{color: "red"}}>*</span></span>
+                                    </div>
+                                    <span id="id-err"></span>
+                                </div>
+                            </div>
+                            <div className="user-input-wrp">
                                 <br/>
-                                <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="website" value=""/>
-                                <span class="floating-label">Password <span style={{color: "red"}}>*</span></span>
+                                <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="website" value=""/>
+                                <span className="floating-label">Password <span style={{color: "red"}}>*</span></span>
                             </div>
-                            <div class="user-input-wrp">
+                            <div className="user-input-wrp">
                                 <br/>
-                                <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="website" value=""/>
-                                <span class="floating-label">Confirm Password <span style={{color: "red"}}>*</span></span>
+                                <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="website" value=""/>
+                                <span className="floating-label">Confirm Password <span style={{color: "red"}}>*</span></span>
                             </div>
                             <span id="id-err"></span>
                         </div>
-                        <p>have an account? <span onClick={activity('login')}>Login here</span></p>
-                        <input type="submit" value="register" name="contact" class="contact-btn"/>
+                        <p>have an account? <span onClick={()=>{setActive('login')}}>Login here</span></p>
+                        <input type="submit" value="register" name="contact" className="contact-btn"/>
                     </form>
                     <form action="contact.php" method="POST" className={(active==='login')?"login-form":"hide-activity"}>
                         <h3>Login</h3>
-                        <div class="cont-group">
-                            <div class="user-input-wrp">
+                        <div className="cont-group">
+                            <div className="user-input-wrp">
                                 <br/>
-                                <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="name" value=""/>
-                                <span class="floating-label">Registration Number <span style={{color: "red"}}>*</span></span>
+                                <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="name" value=""/>
+                                <span className="floating-label">Registration Number <span style={{color: "red"}}>*</span></span>
                             </div>
                             <span id="id-err"></span>
-                            <div class="user-input-wrp">
+                            <div className="user-input-wrp">
                                 <br/>
-                                <input id="id-input" type="text" onkeyup="this.setAttribute('value', this.value);" class="inputText" name="website" value=""/>
-                                <span class="floating-label">Password <span style={{color: "red"}}>*</span></span>
+                                <input id="id-input" type="text" onKeyUp="this.setAttribute('value', this.value);" className="inputText" name="website" value=""/>
+                                <span className="floating-label">Password <span style={{color: "red"}}>*</span></span>
                             </div>
                             <span id="id-err"></span>
                         </div>
-                        <input type="submit" value="login" name="contact" class="contact-btn"/>
+                        <p>don't have an account? <span onClick={()=>{setActive('register')}}>Register here</span></p>
+                        <input type="submit" value="login" name="contact" className="contact-btn"/>
                     </form>
                 </div>
             </div>
@@ -117,4 +115,4 @@ const auth = () => {
   )
 }
 
-export default auth
+export default Auth
