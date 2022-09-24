5f412bb14c6d15c9d7197356e7217eb9286e0d85
DennisRono <finnneron1@gmail.com>
2022-09-24 16:20:02 +0300

    dark birth height ring cup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,34 @@
 import React, { Fragment } from 'react'
 import '../styles/css/header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <Fragment>
-        
+        <header className="header">
+          <div className="header-container">
+            <div className="header-brand">
+              <div className="twitter-logo">
+              <Link to="/"></Link>
+              </div>
+              <div className="brand-texts">
+                <Link to="/"><h3>Fee Payment System</h3></Link>
+              </div>
+            </div>
+            <div className="navigation">
+              <nav className="nav">
+                <ul className="nav-list">
+                  <li className="nav-link">
+                    <Link className="nav-red" to="/about">About</Link>
+                  </li>
+                  <li className="nav-link">
+                    <Link className="nav-red" to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
     </Fragment>
   )
 }
