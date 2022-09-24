1eb864ebf8b0bc6749f3982613c87e7eea51e2cc
DennisRono <finnneron1@gmail.com>
2022-09-24 13:30:01 +0300

    we seen greater best no

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react'
+import { api } from '../api/axios'
 
 const Home = () => {
+  const fetchToken = async (reftoken) => {
+    let res = await api('GET', 'auth/token', {refreshToken: reftoken})
+  }
   return (
     <Fragment>
     </Fragment>
