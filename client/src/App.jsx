24d64c150f09647e1db2115fef189a75bc3475f9
DennisRono <finnneron1@gmail.com>
2022-09-24 12:00:01 +0300

    seems store spin route no

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './routes/Home'
 import Auth from './routes/Auth'
 
 const App = () => {
   return (
     <Fragment>
-      <Auth />
+      <div className="App">
+        <div className="contents">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/auth" element={<Auth />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </Fragment>
   )
 }
--- client/src/routes/NotFound.jsx
+++ client/src/routes/NotFound.jsx
@@ -1,10 +1,31 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, useState } from 'react'
+import '../styles/css/notfound.css'
+import { Link } from "react-router-dom"
 
-const NotFound = () => {
+const NotFound = (props) => {
+    const [seconds, setSeconds ] =  useState(10);
+    useEffect(()=>{
+        let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            } else {
+                window.location.href = "/"
+            }
+        }, 1000)
+        return ()=> {
+            clearInterval(myInterval);
+        };
+    });
   return (
-    <Fragment>
-        
-    </Fragment>
+    <div>
+        <div className="notfoundWrapper">
+            <div className="notFound">
+                <p className="fourNotFound">404</p>
+                <p className="notFoundTexts">We can't seem to find the page you are looking for.<br /> We will try to redirect you automatically in { seconds } seconds.</p>
+                <Link className="notFoundLink" to="/">back home</Link>
+            </div>           
+        </div>
+    </div>
   )
 }
 
