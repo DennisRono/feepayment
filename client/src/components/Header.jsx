9fb8134df89454c60af2a76ad97a222f386fcf98
DennisRono <finnneron1@gmail.com>
2022-09-24 16:10:00 +0300

    winter planet show plan express

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import '../styles/css/header.css'
 
 const Header = () => {
   return (
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -2,30 +2,32 @@ import React, { Fragment, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
 import { api } from '../api/axios'
 import { getJwtToken, getRefreshToken, setJwtToken } from '../includes/session'
+import Header from '../components/Header'
 
 const Home = () => {
   let navigate = useNavigate()
-  const vToken = async (t, refT) => {
-    let ver =  await api('POST', 'auth/verifytoken', {token: t, refreshToken: getRefreshToken()})
-    if(ver.type==='Error'){
-      return navigate("/auth")
-    } else {
-      setJwtToken(ver.authToken)
-    }
-  }
   // check is user is logged in
   const jwtToken = getJwtToken()
   const refTok = getRefreshToken()
   useEffect(()=>{
+    const vToken = async (t, refT) => {
+      let ver =  await api('POST', 'auth/verifytoken', {token: t, refreshToken: getRefreshToken()})
+      if(ver.type==='Error'){
+        return navigate("/auth")
+      } else {
+        setJwtToken(ver.authToken)
+      }
+    }
     if (!jwtToken && !refTok) {
       return navigate("/auth")
     } else {
       vToken(jwtToken)
     }
-  }, [jwtToken, navigate])
+  }, [jwtToken, navigate, refTok])
 
   return (
     <Fragment>
+      <Header />
     </Fragment>
   )
 }
